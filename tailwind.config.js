67bce74fbb5cd86d227cd3c06d6ceaec20924756
HHJ-S2 <berdle0122@gmail.com>
2025-08-18 09:34:57 +0900

    feat: 커스텀 폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
     screens: {
-      'sm': '38rem',
-    }
+      sm: "38rem",
+    },
+    fontFamily: {
+      Pretendard: ["Pretendard"],
+    },
   },
   plugins: [],
-  darkMode: 'class'
-}
+  darkMode: "class",
+};
